6d04c7e7d90afd566079764ac68378ca9eeb7042
Gustavo Martins <gm0635353@gmail.com>
2024-11-27 22:55:09 -0300

    send emails
    enviado emails de forma semi automática

---
+++ send_email.py
@@ -0,0 +1,31 @@
+import smtplib
+import email.message
+
+# srfrhasukyweewsd
+from_email = input("Digite seu email:")
+destiny_email = input("Digite o email de quem vai receber:")
+subject = input("Digite o assunto do email:")
+content = input("Digite a mensagem do email em forma de texto ou html:")
+app_password = input("Digite a sua senha de app:")
+
+
+def send_email():
+    email_body = f"{content}"
+
+    msg = email.message.Message()
+    msg['Subject'] = f"{subject}"
+    msg['From'] = f"{from_email}"
+    msg['To'] = f"{destiny_email}"
+    password = f'{app_password}'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(email_body)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+
+send_email()
